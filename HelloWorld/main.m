2c6e001516e69fb138cb105ab06913af9df3e81a
Weijie He <heweijie@gih-d-12076.local>
2018-06-04 16:02:36 +0800

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Weijie He on 2018/6/4.
+//  Copyright © 2018年 baostorm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
